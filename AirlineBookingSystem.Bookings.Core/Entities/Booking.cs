c33b9932a7150dc92682894c134d3b917e24701a
Kethavath Gopal <kethavath.gopal@valuemomentum.com>
2026-05-06 14:12:44 +0530

    Fix issues and update .gitignore

---
+++ AirlineBookingSystem.Bookings.Core/Entities/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Bookings.Core.Entities
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public Guid FlightId { get; set; }
+        public string Name { get; set; }
+        public string SeatNumber { get; set; }
+        public DateTime BookingDate { get; set; }
+
+
+    }
+}
